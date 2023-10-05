627c473e9290e835b3d1ba45a0c94b972b036d29
Aaron Silvas <aaronsilvas@gmail.com>
2023-10-05 10:46:55 -0700

    promises

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-import path from 'path';
-import fs from 'fs';
-import mkdirp from 'mkdirp';
-import crypto from 'crypto';
-import rimraf from 'rimraf';
+const path = require('path');
+const fs = require('fs/promises');
+const crypto = require('crypto');
 
-export default class BlobbyFS {
+module.exports = class BlobbyFS {
   constructor(opts) {
     this.options = opts || {};
     if (!this.options.path) throw new Error('BlobbyFS requires `path` option');
   }
 
-  fetchInfo(fileKey, opts, cb) {
-    if (typeof opts === 'function') {
-      cb = opts;
-      opts = null;
-    }
-    opts = opts || { };
-
+  fetchInfo(fileKey, opts = {}) {
     const absPath = path.resolve(path.join(this.options.path, fileKey));
-    fs.stat(absPath, (err, stats) => {
-      if (err) return void cb(err);
+    return fs.stat(absPath).then(stats => {
+      if (!stats.isFile()) throw new Error('Requested path is not a file');
 
-      if (!stats.isFile()) return void cb(new Error('Requested path is not a file'));
-
-      cb(null, { LastModified: stats.mtime, Size: stats.size });
+      return { LastModified: stats.mtime, Size: stats.size };
     });
   }
 
@@ -31,23 +21,17 @@ export default class BlobbyFS {
     fileKey: unique id for storage
     opts: future
    */
-  fetch(fileKey, opts, cb) {
-    if (typeof opts === 'function') {
-      cb = opts;
-      opts = null;
-    }
-    opts = opts || { };
-
+  async fetch(fileKey) {
     const absPath = path.resolve(path.join(this.options.path, fileKey));
-    fs.readFile(absPath, { encoding: null }, (err, data) => {
-      if (err) return void cb(err);
+    const data = await fs.readFile(absPath, { encoding: null });
 
-      // compute etag
-      const ETag = crypto.createHash('md5').update(data).digest('hex');
+    // compute etag
+    const ETag = crypto.createHash('md5').update(data).digest('hex');
 
-      const stats = fs.statSync(absPath);
-      cb(null, { ETag, LastModified: stats.mtime, Size: stats.size }, data);
-    });
+    const stats = await fs.stat(absPath);
+
+    // tuple contains headers and data
+    return [{ ETag, LastModified: stats.mtime, Size: stats.size }, data];
   }
 
   /*
@@ -58,74 +42,48 @@ export default class BlobbyFS {
    file.headers.LastModified: If specified, will force this value on newly written files
    opts: future
    */
-  store(fileKey, file, opts, cb) {
-    if (typeof opts === 'function') {
-      cb = opts;
-      opts = null;
-    }
-    opts = opts || {};
-
+  async store(fileKey, file, opts = {}) {
     const { buffer, headers = {} } = file;
     const absPath = path.resolve(path.join(this.options.path, fileKey));
     const $this = this;
-    fs.writeFile(absPath, buffer, {}, err => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // attempt to create directory (recursively)
-          return void mkdirp(path.dirname(absPath), err => {
-            if (err) return void cb(err); // dir creation failed
+    await fs.writeFile(absPath, buffer, {}).catch(async err => {
+      if (err.code === 'ENOENT') {
+        await fs.mkdir(path.dirname(absPath), { recursive: true });
 
-            // success, so lets try to store again
-            $this.store(fileKey, file, opts, cb);
-          });
-        }
-
-        return void cb(err);
-      }
-
-      // compute etag
-      headers.ETag = crypto.createHash('md5').update(buffer).digest('hex');
-
-      if (typeof headers.LastModified === 'object') {
-        // if LastModified is set, apply to target object for proper syncing
-        fs.utimes(absPath,
-          Date.now() / 1000 /* atime: Access Time */,
-          headers.LastModified.getTime() / 1000 /* mtime: Modified Time */
-          , () => cb(null, headers)
-        );
+        // success, so lets try to store again
+        return $this.store(fileKey, file, opts);
       } else {
-        // otherwise return success now
-        cb(null, headers);
+        throw err;
       }
     });
+
+    // compute etag
+    headers.ETag = crypto.createHash('md5').update(buffer).digest('hex');
+
+    if (typeof headers.LastModified === 'object') {
+      // if LastModified is set, apply to target object for proper syncing
+      await fs.utimes(absPath,
+        Date.now() / 1000 /* atime: Access Time */,
+        headers.LastModified.getTime() / 1000 /* mtime: Modified Time */
+      );
+    }
+    return headers;
   }
 
   /*
    fileKey: unique id for storage
    */
-  remove(fileKey, cb) {
+  remove(fileKey) {
     const absPath = path.resolve(path.join(this.options.path, fileKey));
-    fs.unlink(absPath, err => {
-      if (err) {
-        return void cb(err);
-      }
-
-      cb();
-    });
+    return fs.unlink(absPath);
   }
 
   /*
    dir: unique id for storage
    */
-  removeDirectory(dir, cb) {
+  removeDirectory(dir) {
     const absPath = path.resolve(path.join(this.options.path, dir)) + '/';
-    rimraf(absPath, { glob: false }, err => {
-      if (err) {
-        return void cb(err);
-      }
-
-      cb();
-    });
+    return fs.rmdir(absPath, { recursive: true });
   }
 
   /* supported options:
@@ -134,66 +92,46 @@ export default class BlobbyFS {
    opts.lastKey: if requesting beyond maxKeys (paging)
    opts.maxKeys: ignored for this storage client
    opts.deepQuery: not supported for this storage client
-   cb(err, files, dirs, lastKey) - Callback fn
-   cb.err: Error if any
-   cb.files: An array of files: { Key, LastModified, ETag, Size, ... }
-   cb.dirs: An array of dirs: [ 'a', 'b', 'c' ]
-   cb.lastKey: An identifier to permit retrieval of next page of results, ala: 'abc'
   */
-  list(dir, opts, cb) {
-    if (typeof opts === 'function') {
-      cb = opts;
-      opts = null;
-    }
-    opts = opts || {};
-
+  list(dir, opts = {}) {
     if (!opts.deepQuery) {
       // simple mode, query current directory
 
-      const simpleDir = path.resolve(path.join(this.options.path, dir));
-
-      return void query(this.options.path, dir, cb);
+      return query(this.options.path, dir);
     }
 
     // otherwise use deep query logic for use with querying entire tree
-    deepQuery(this.options.path, dir, opts.lastKey, cb);
+    return deepQuery(this.options.path, dir, opts.lastKey);
   }
 }
 
-function query(root, currentDir, opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts;
-    opts = null;
-  }
-  opts = opts || {};
+async function query(root, currentDir, opts = {}) {
   opts.lastDir = opts.lastDir || '';
   const absPath = path.join(root, currentDir);
-  fs.readdir(absPath, (err, dirContents) => {
-    if (err) return void cb(err);
+  const dirContents = await fs.readdir(absPath);
 
-    const dirs = [];
-    const files = [];
-    dirContents.forEach(name => {
-      const Key = path.join(currentDir, name);
-      if (name <= opts.lastDir) {
-        // Ignore/filter anything less than or equal to lastDir.
-        // This is not only an optimization, but is a required pattern
-        // to avoid returning the same file more than once.
-        return;
-      }
-      const stats = fs.statSync(path.join(root, Key));
-      if (stats.isDirectory()) {
-        dirs.push({Key});
-        return;
-      }
+  const dirs = [];
+  const files = [];
+  for (let name of dirContents) {
+    const Key = path.join(currentDir, name);
+    if (name <= opts.lastDir) {
+      // Ignore/filter anything less than or equal to lastDir.
+      // This is not only an optimization, but is a required pattern
+      // to avoid returning the same file more than once.
+      continue;
+    }
+    const stats = await fs.stat(path.join(root, Key));
+    if (stats.isDirectory()) {
+      dirs.push({ Key });
+      continue;
+    }
 
-      if (!opts.ignoreFiles) {
-        files.push({Key, LastModified: stats.mtime, Size: stats.size });
-      }
-    });
+    if (!opts.ignoreFiles) {
+      files.push({ Key, LastModified: stats.mtime, Size: stats.size });
+    }
+  }
 
-    cb(null, files.sort(sortByKey), dirs.sort(sortByKey));
-  });
+  return [files.sort(sortByKey), dirs.sort(sortByKey)];
 }
 
 function sortByKey(a, b) {
@@ -225,14 +163,12 @@ function sortByKey(a, b) {
   -a:.
   (eof, no more dirs after `a`)
 */
-function deepQuery(root, currentDir, lastKey, cb) {
+async function deepQuery(root, currentDir, lastKey) {
   if (!lastKey) {
     // initial query
-    return void query(root, currentDir, (err, files, dirs) => {
-      if (err) return void cb(err);
-
+    return query(root, currentDir).then(([files, dirs]) => {
       const lastKey = dirs.length > 0 ? buildLastKeyRight(currentDir, path.join(currentDir, dirs[0].Key)) : null;
-      cb(null, files, [], lastKey);
+      return [files, [], lastKey];
     });
   }
 
@@ -241,17 +177,15 @@ function deepQuery(root, currentDir, lastKey, cb) {
   const lastDir = keyInfo.leftToRight === false ? path.basename(keyInfo.lastDir) : null;
   const ignoreFiles = !keyInfo.leftToRight; // ignore files in nextDir if going backwards
   //console.log(`querying ${keyInfo.nextDir}, leftToRight:${keyInfo.leftToRight}, lastDir:${lastDir}, ignoreFiles:${ignoreFiles}...`)
-  query(root, keyInfo.nextDir, {lastDir, ignoreFiles}, (err, files, dirs) => {
-    if (err) return void cb(err); // we're done, bail
+  const [files, dirs] = await query(root, keyInfo.nextDir, { lastDir, ignoreFiles });
 
-    if (dirs.length === 0) {
-      // no directories to continue down, go back
-      cb(null, files, [], buildLastKeyLeft(keyInfo.nextDir));
-    } else {
-      // contains directories, so continue searching
-      cb(null, files, [], buildLastKeyRight(keyInfo.nextDir, dirs[0].Key));
-    }
-  });
+  if (dirs.length === 0) {
+    // no directories to continue down, go back
+    return [files, [], buildLastKeyLeft(keyInfo.nextDir)];
+  } else {
+    // contains directories, so continue searching
+    return [files, [], buildLastKeyRight(keyInfo.nextDir, dirs[0].Key)];
+  }
 }
 
 function getLastKeyInfo(lastKey) {
